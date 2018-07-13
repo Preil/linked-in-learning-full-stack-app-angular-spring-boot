226634ba79ecf32a1bd259163f4b1591bdf46e1e
preil <preil2000@gmail.com>
2018-07-13 17:31:06 +0300

    Configuring application and add JSON object mapper.

--- src/main/java/com/linkedin/learning/linkedinlearningfullstackappangularspringboot/LinkedInLearningFullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/linkedin/learning/linkedinlearningfullstackappangularspringboot/LinkedInLearningFullStackAppAngularSpringBootApplication.java
@@ -1,9 +1,14 @@
 package com.linkedin.learning.linkedinlearningfullstackappangularspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+
 public class LinkedInLearningFullStackAppAngularSpringBootApplication {
 
 	public static void main(String[] args) {
